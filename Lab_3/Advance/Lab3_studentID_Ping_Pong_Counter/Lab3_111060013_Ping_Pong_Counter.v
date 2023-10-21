7ba240f785907616f2a1da22a5da07152c095031
liuutin9 <liuutin9@gmail.com>
2023-10-22 00:46:41 +0800

    Update advance 1

--- Lab_3/Advance/Lab3_studentID_Ping_Pong_Counter/Lab3_111060013_Ping_Pong_Counter.v
+++ Lab_3/Advance/Lab3_studentID_Ping_Pong_Counter/Lab3_111060013_Ping_Pong_Counter.v
@@ -6,61 +6,37 @@ module Ping_Pong_Counter (clk, rst_n, enable, direction, out);
     output reg direction;
     output reg [4-1:0] out;
 
-    reg [3:0] dir_to_enable, enable_to_rst_n, rst_n_to_DFF, dir_from_xor, dir_from_rst_mux, dir_from_enable, dir_from_or;
+    reg [3:0] tmp_out;
+    reg tmp_dir;
 
-    always @ (direction or out) begin
-        dir_from_or = (direction == 1'b1 & out == 4'b1111) | (direction == 1'b0 & out == 4'b0000);
-    end
-
-    always @ (direction or dir_from_or) begin
-        dir_from_xor = dir_from_or ^ direction;
-    end
-
-    always @ (dir_from_xor or enable) begin
-        case (enable)
-            1'b0: dir_from_enable = direction;
-            1'b1: dir_from_enable = dir_from_xor;
-        endcase
-    end
-
-    always @ (dir_from_enable or rst_n) begin
-        case (rst_n)
-            1'b0: dir_from_rst_mux = 1'b1;
-            1'b1: dir_from_rst_mux = dir_from_xor;
-        endcase
-    end
-
-    always @ (posedge clk) begin
-        direction <= dir_from_rst_mux;
-    end
-
-    // Mux: selected by direction
-    always @ (direction or out) begin
-        case (direction)
-            1'b0: dir_to_enable = (out == 4'b0000) ? out + 1 : out - 1;
-            1'b1: dir_to_enable = (out == 4'b1111) ? out - 1 : out + 1;
-        endcase
-    end
-
-    // Mux: selected by enable
-    always @ (dir_to_enable or out or enable) begin
-        case (enable)
-            1'b0: enable_to_rst_n = out;
-            1'b1: enable_to_rst_n = dir_to_enable;
-        endcase
-    end
-
-    // Mux: selected by rst_n
-    always @ (rst_n or enable_to_rst_n) begin
-        case (rst_n)
-            1'b0: rst_n_to_DFF = 4'b0000;
-            1'b1: rst_n_to_DFF = enable_to_rst_n;
-        endcase
+    // Mux
+    always @ (direction or out or enable or rst_n) begin
+        if (rst_n == 1'b0) tmp_dir = 1'b1;
+        else if (enable && ((direction == 1'b1 && out == 4'b1111) || (direction == 1'b0 && out == 4'b0000)))
+            tmp_dir = ~direction;
+        else tmp_dir = direction;
     end
 
     // DFF
     always @ (posedge clk) begin
-        out <= rst_n_to_DFF;
+        direction <= tmp_dir;
+    end
+
+    // Mux
+    always @ (direction or out or enable or rst_n) begin
+        if (rst_n == 1'b0) tmp_out = 4'b0000;
+        else if (enable == 1'b1) begin
+            case (direction)
+                1'b0: tmp_out = (out == 4'b0000) ? out + 1 : out - 1;
+                1'b1: tmp_out = (out == 4'b1111) ? out - 1 : out + 1;
+            endcase
+        end
+        else tmp_out = out;
+    end
+
+    // DFF
+    always @ (posedge clk) begin
+        out <= tmp_out;
     end
 
 endmodule
--- Lab_3/Advance/Lab3_studentID_Ping_Pong_Counter/Lab3_studentID_Ping_Pong_Counter.sim/sim_1/behav/xsim/xsim.dir/Lab3_111060013_Ping_Pong_Counter_t_behav/webtalk/xsim_webtalk.tcl
+++ Lab_3/Advance/Lab3_studentID_Ping_Pong_Counter/Lab3_studentID_Ping_Pong_Counter.sim/sim_1/behav/xsim/xsim.dir/Lab3_111060013_Ping_Pong_Counter_t_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Logic_Design_Lab/Lab_3/Advance/Lab3_studentID_Ping_Pong_Counter/Lab3_studentID_Ping_Pong_Counter.sim/sim_1/behav/xsim/xsim.dir/Lab3_111060013_Ping_Pong_Counter_t_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Fri Oct 20 13:54:59 2023" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Sun Oct 22 00:45:17 2023" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3064766" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "1bde974c-ecb9-4b54-bb81-4da72a94bdc2" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "3d290744fedc4d77a53a99ca0d319bd7" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "24" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "30" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "11th Gen Intel(R) Core(TM) i5-1155G7 @ 2.50GHz" -context "user_environment"
@@ -24,9 +24,9 @@ webtalk_add_data -client project -key system_ram -value "16.000 GB" -context "us
 webtalk_register_client -client xsim
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "203 ns" -context "xsim\\usage"
+webtalk_add_data -client xsim -key runtime -value "153 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.01_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "7412_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 1264652622 -regid "" -xml C:/Logic_Design_Lab/Lab_3/Advance/Lab3_studentID_Ping_Pong_Counter/Lab3_studentID_Ping_Pong_Counter.sim/sim_1/behav/xsim/xsim.dir/Lab3_111060013_Ping_Pong_Counter_t_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Logic_Design_Lab/Lab_3/Advance/Lab3_studentID_Ping_Pong_Counter/Lab3_studentID_Ping_Pong_Counter.sim/sim_1/behav/xsim/xsim.dir/Lab3_111060013_Ping_Pong_Counter_t_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Logic_Design_Lab/Lab_3/Advance/Lab3_studentID_Ping_Pong_Counter/Lab3_studentID_Ping_Pong_Counter.sim/sim_1/behav/xsim/xsim.dir/Lab3_111060013_Ping_Pong_Counter_t_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.00_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "7396_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 1840575196 -regid "" -xml C:/Logic_Design_Lab/Lab_3/Advance/Lab3_studentID_Ping_Pong_Counter/Lab3_studentID_Ping_Pong_Counter.sim/sim_1/behav/xsim/xsim.dir/Lab3_111060013_Ping_Pong_Counter_t_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Logic_Design_Lab/Lab_3/Advance/Lab3_studentID_Ping_Pong_Counter/Lab3_studentID_Ping_Pong_Counter.sim/sim_1/behav/xsim/xsim.dir/Lab3_111060013_Ping_Pong_Counter_t_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Logic_Design_Lab/Lab_3/Advance/Lab3_studentID_Ping_Pong_Counter/Lab3_studentID_Ping_Pong_Counter.sim/sim_1/behav/xsim/xsim.dir/Lab3_111060013_Ping_Pong_Counter_t_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
