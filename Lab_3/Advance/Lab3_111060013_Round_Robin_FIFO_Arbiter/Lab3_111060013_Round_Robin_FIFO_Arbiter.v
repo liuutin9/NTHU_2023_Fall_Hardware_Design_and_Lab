069cb38edd1f77fa96dd5e3eb7292ad60700deb6
liuutin9 <136707519+liuutin9@users.noreply.github.com>
2023-10-24 16:07:22 +0800

    Update Lab3_111060013_Round_Robin_FIFO_Arbiter.v

--- Lab_3/Advance/Lab3_111060013_Round_Robin_FIFO_Arbiter/Lab3_111060013_Round_Robin_FIFO_Arbiter.v
+++ Lab_3/Advance/Lab3_111060013_Round_Robin_FIFO_Arbiter/Lab3_111060013_Round_Robin_FIFO_Arbiter.v
@@ -13,57 +13,49 @@ module Round_Robin_FIFO_Arbiter(clk, rst_n, wen, a, b, c, d, dout, valid);
     reg [3:0] ren, rst_ren;
     reg tmp_valid, rst_valid;
     reg[7:0] tmp_dout, rst_dout, valid_dout;
+    wire [3:0] real_ren;
+
+    assign real_ren = !wen && ren;
 
     // dout
-    always @ (ren or QA_Mux or QB_Mux or QC_Mux or QD_Mux) begin
-        case (ren)
-            4'b0001: tmp_dout = QA_Mux;
-            4'b0010: tmp_dout = QB_Mux;
-            4'b0100: tmp_dout = QC_Mux;
-            4'b1000: tmp_dout = QD_Mux;
-            default: tmp_dout = dout;
-        endcase
-    end
-
-    always @ (tmp_dout or rst_n) begin
-        case (rst_n)
-            1'b0: rst_dout = 8'b00000000;
-            1'b1: rst_dout = tmp_dout;
-        endcase
+    always @ (*) begin
+        if (!rst_n) begin
+            tmp_dout = 8'd0;
+        end
+        else begin
+            if (tmp_valid) begin
+                case (ren)
+                    4'b0001: tmp_dout = QA_Mux;
+                    4'b0010: tmp_dout = QB_Mux;
+                    4'b0100: tmp_dout = QC_Mux;
+                    4'b1000: tmp_dout = QD_Mux;
+                    default: tmp_dout = dout;
+                endcase
+            end
+            else tmp_dout = 8'd0;
+        end
     end
 
     always @ (posedge clk) begin
-        valid_dout <= rst_dout;
-    end
-
-    always @ (valid_dout or valid) begin
-        case (valid)
-            1'b0: dout = 8'b00000000;
-            1'b1: dout = valid_dout;
-        endcase
+        dout <= tmp_dout;
     end
 
     // valid
-    always @ (ren or wen or err) begin
-        tmp_valid = (err == 4'b0000) && ((ren & wen) == 4'b0000);
-    end
-
-    always @ (tmp_valid or rst_n) begin
-        case (rst_n)
-            1'b0: rst_valid = 1'b0;
-            1'b1: rst_valid = tmp_valid;
-        endcase
+    always @ (*) begin
+        if (!rst_n) tmp_valid = 1'b0;
+        else tmp_valid = (err == 4'b0000) && ((ren & wen) == 4'b0000);
     end
 
     always @ (posedge clk) begin
-        valid <= rst_valid;
+        valid <= tmp_valid;
     end
 
     // count ren
-    always @ (rst_n or ren) begin
+    always @ (*) begin
         case (rst_n)
             1'b0: rst_ren = 4'b0001;
             1'b1: rst_ren = {ren[2:0], ren[3]};
+            default: rst_ren = ren;
         endcase
     end
 
@@ -85,7 +77,7 @@ module Round_Robin_FIFO_Arbiter(clk, rst_n, wen, a, b, c, d, dout, valid);
         .clk(clk),
         .rst_n(rst_n),
         .wen(wen[1]),
-        .ren(ren[1]),
+        .ren(real_ren[1]),
         .din(b),
         .dout(QB_Mux),
         .error(err[1])
@@ -95,7 +87,7 @@ module Round_Robin_FIFO_Arbiter(clk, rst_n, wen, a, b, c, d, dout, valid);
         .clk(clk),
         .rst_n(rst_n),
         .wen(wen[2]),
-        .ren(ren[2]),
+        .ren(real_ren[2]),
         .din(c),
         .dout(QC_Mux),
         .error(err[2])
@@ -105,7 +97,7 @@ module Round_Robin_FIFO_Arbiter(clk, rst_n, wen, a, b, c, d, dout, valid);
         .clk(clk),
         .rst_n(rst_n),
         .wen(wen[3]),
-        .ren(ren[3]),
+        .ren(real_ren[3]),
         .din(d),
         .dout(QD_Mux),
         .error(err[3])
@@ -121,215 +113,125 @@ module FIFO_8(clk, rst_n, wen, ren, din, dout, error);
     output reg [8-1:0] dout;
     output reg error;
 
-    reg [3:0] rp, rp_to_rst_n, rst_n_to_rp_DFF;
-    reg [3:0] wp, wp_to_rst_n, rst_n_to_wp_DFF;
-    reg [7:0] queue [7:0];
-    reg [7:0] Mux7, Mux6, Mux5, Mux4, Mux3, Mux2, Mux1, Mux0;
+    reg [3:0] rp, tmp_rp;
+    reg [3:0] wp, tmp_wp;
     reg [7:0] DFF [7:0];
-    reg [7:0] Rst7, Rst6, Rst5, Rst4, Rst3, Rst2, Rst1, Rst0;
-    reg [7:0] tmp_dout, tmp_din;
+    reg [7:0] tmp_DFF [7:0];
+    reg [7:0] tmp_out;
+    reg tmp_err;
 
-    // read pointer
-    always @ (rp or ren or wen) begin
-        case ({wen, ren})
-            2'b00: rp_to_rst_n = rp;
-            2'b01: rp_to_rst_n = (rp == 4'b1000) ? 4'b1000 : rp + 4'b0001;
-            2'b10: rp_to_rst_n = (rp == 4'b0000) ? 4'b0000 : rp - 4'b0001;
-            2'b11: rp_to_rst_n = (rp == 4'b0000) ? 4'b0000 : rp - 4'b0001;
-        endcase
-    end
-
-    always @ (rp_to_rst_n or rst_n) begin
-        case (rst_n)
-            1'b0: rst_n_to_rp_DFF = 4'b1000;
-            1'b1: rst_n_to_rp_DFF = rp_to_rst_n;
-        endcase
-    end
-
-    always @ (posedge clk) begin
-        rp <= rst_n_to_rp_DFF;
-    end
-
-    // write pointer
-    always @ (wp or ren or wen) begin
-        case ({wen, ren})
-            2'b00: wp_to_rst_n = wp;
-            2'b01: wp_to_rst_n = (wp == 4'b0111) ? 4'b0111 : wp + 4'b0001;
-            2'b10: wp_to_rst_n = (wp == 4'b1111) ? 4'b1111 : wp - 4'b0001;
-            2'b11: wp_to_rst_n = (wp == 4'b1111) ? 4'b1111 : wp - 4'b0001;
-        endcase
-    end
-
-    always @ (wp_to_rst_n or rst_n) begin
-        case (rst_n)
-            1'b0: rst_n_to_wp_DFF = 4'b0111;
-            1'b1: rst_n_to_wp_DFF = wp_to_rst_n;
-        endcase
-    end
-
-    always @ (posedge clk) begin
-        wp <= rst_n_to_wp_DFF;
-    end
-
-    // 7th bit
-    always @ (wp or wen or Mux7 or din or DFF[7]) begin
-        case ({wen, wp != 4'b1111})
-            default: Mux7 = DFF[7];
-            2'b11: Mux7 = din;
-        endcase
-    end
-
-    always @ (rst_n or Mux7) begin
-        case (rst_n)
-            1'b0: Rst7 = 8'b00000000;
-            1'b1: Rst7 = Mux7;
-        endcase
-    end
-
-    always @ (posedge clk) begin
-        DFF[7] <= Rst7;
-    end
-
-    // 6th bit
-    always @ (wp or wen or Mux6 or DFF[7] or DFF[6]) begin
-        case ({wen, wp != 4'b1111})
-            default: Mux6 = DFF[6];
-            2'b11: Mux6 = DFF[7];
-        endcase
-    end
-
-    always @ (rst_n or Mux6) begin
-        case (rst_n)
-            1'b0: Rst6 = 8'b00000000;
-            1'b1: Rst6 = Mux6;
-        endcase
-    end
-
-    always @ (posedge clk) begin
-        DFF[6] <= Rst6;
-    end
-
-    // 5th bit
-    always @ (wp or wen or Mux5 or DFF[6] or DFF[5]) begin
-        case ({wen, wp != 4'b1111})
-            default: Mux5 = DFF[5];
-            2'b11: Mux5 = DFF[6];
-        endcase
-    end
-
-    always @ (rst_n or Mux5) begin
-        case (rst_n)
-            1'b0: Rst5 = 8'b00000000;
-            1'b1: Rst5 = Mux5;
-        endcase
-    end
-
-    always @ (posedge clk) begin
-        DFF[5] <= Rst5;
-    end
-
-    // 4th bit
-    always @ (wp or wen or Mux4 or DFF[5] or DFF[4]) begin
-        case ({wen, wp != 4'b1111})
-            default: Mux4 = DFF[4];
-            2'b11: Mux4 = DFF[5];
-        endcase
-    end
-
-    always @ (rst_n or Mux4) begin
-        case (rst_n)
-            1'b0: Rst4 = 8'b00000000;
-            1'b1: Rst4 = Mux4;
-        endcase
-    end
-
-    always @ (posedge clk) begin
-        DFF[4] <= Rst4;
-    end
-
-    // 3rd bit
-    always @ (wp or wen or Mux3 or DFF[4] or DFF[3]) begin
-        case ({wen, wp != 4'b1111})
-            default: Mux3 = DFF[3];
-            2'b11: Mux3 = DFF[4];
-        endcase
-    end
-
-    always @ (rst_n or Mux3) begin
-        case (rst_n)
-            1'b0: Rst3 = 8'b00000000;
-            1'b1: Rst3 = Mux3;
-        endcase
-    end
-
-    always @ (posedge clk) begin
-        DFF[3] <= Rst3;
-    end
-
-    // 2nd bit
-    always @ (wp or wen or Mux2 or DFF[3] or DFF[2]) begin
-        case ({wen, wp != 4'b1111})
-            default: Mux2 = DFF[2];
-            2'b11: Mux2 = DFF[3];
-        endcase
-    end
-
-    always @ (rst_n or Mux2) begin
-        case (rst_n)
-            1'b0: Rst2 = 8'b00000000;
-            1'b1: Rst2 = Mux2;
-        endcase
-    end
-
-    always @ (posedge clk) begin
-        DFF[2] <= Rst2;
-    end
-
-    // 1st bit
-    always @ (wp or wen or Mux1 or DFF[2] or DFF[1]) begin
-        case ({wen, wp != 4'b1111})
-            default: Mux1 = DFF[1];
-            2'b11: Mux1 = DFF[2];
-        endcase
-    end
-
-    always @ (rst_n or Mux1) begin
-        case (rst_n)
-            1'b0: Rst1 = 8'b00000000;
-            1'b1: Rst1 = Mux1;
-        endcase
-    end
-
-    always @ (posedge clk) begin
-        DFF[1] <= Rst1;
-    end
-
-    // 0th bit
-    always @ (wp or wen or Mux0 or DFF[1] or DFF[0]) begin
-        case ({wen, wp != 4'b1111})
-            default: Mux0 = DFF[0];
-            2'b11: Mux0 = DFF[1];
-        endcase
-    end
-
-    always @ (rst_n or Mux0) begin
-        case (rst_n)
-            1'b0: Rst0 = 8'b00000000;
-            1'b1: Rst0 = Mux0;
-        endcase
-    end
-
-    always @ (posedge clk) begin
-        DFF[0] <= Rst0;
-    end
-
-    // read enable
     always @ (*) begin
-        dout = DFF[rp];
+        if (!rst_n) begin
+            tmp_DFF[7] = 8'd0;
+            tmp_DFF[6] = 8'd0;
+            tmp_DFF[5] = 8'd0;
+            tmp_DFF[4] = 8'd0;
+            tmp_DFF[3] = 8'd0;
+            tmp_DFF[2] = 8'd0;
+            tmp_DFF[1] = 8'd0;
+            tmp_DFF[0] = 8'd0;
+            tmp_rp = 4'b1000;
+            tmp_wp = 4'b0111;
+        end
+        else begin
+            if (ren) begin
+                if (!tmp_err) begin
+                    tmp_DFF[7] = DFF[7];
+                    tmp_DFF[6] = DFF[6];
+                    tmp_DFF[5] = DFF[5];
+                    tmp_DFF[4] = DFF[4];
+                    tmp_DFF[3] = DFF[3];
+                    tmp_DFF[2] = DFF[2];
+                    tmp_DFF[1] = DFF[1];
+                    tmp_DFF[0] = DFF[0];
+                    tmp_rp = rp + 4'b0001;
+                    tmp_wp = wp + 4'b0001;
+                end
+                else begin
+                    tmp_DFF[7] = DFF[7];
+                    tmp_DFF[6] = DFF[6];
+                    tmp_DFF[5] = DFF[5];
+                    tmp_DFF[4] = DFF[4];
+                    tmp_DFF[3] = DFF[3];
+                    tmp_DFF[2] = DFF[2];
+                    tmp_DFF[1] = DFF[1];
+                    tmp_DFF[0] = DFF[0];
+                    tmp_rp = rp;
+                    tmp_wp = wp;
+                end
+            end
+            else begin
+                if (wen) begin
+                    if (!tmp_err) begin
+                        tmp_DFF[7] = din;
+                        tmp_DFF[6] = DFF[7];
+                        tmp_DFF[5] = DFF[6];
+                        tmp_DFF[4] = DFF[5];
+                        tmp_DFF[3] = DFF[4];
+                        tmp_DFF[2] = DFF[3];
+                        tmp_DFF[1] = DFF[2];
+                        tmp_DFF[0] = DFF[1];
+                        tmp_rp = rp - 4'b0001;
+                        tmp_wp = wp - 4'b0001;
+                    end
+                    else begin
+                        tmp_DFF[7] = DFF[7];
+                        tmp_DFF[6] = DFF[6];
+                        tmp_DFF[5] = DFF[5];
+                        tmp_DFF[4] = DFF[4];
+                        tmp_DFF[3] = DFF[3];
+                        tmp_DFF[2] = DFF[2];
+                        tmp_DFF[1] = DFF[1];
+                        tmp_DFF[0] = DFF[0];
+                        tmp_rp = rp;
+                        tmp_wp = wp;
+                    end
+                end
+                else begin
+                    tmp_DFF[7] = DFF[7];
+                    tmp_DFF[6] = DFF[6];
+                    tmp_DFF[5] = DFF[5];
+                    tmp_DFF[4] = DFF[4];
+                    tmp_DFF[3] = DFF[3];
+                    tmp_DFF[2] = DFF[2];
+                    tmp_DFF[1] = DFF[1];
+                    tmp_DFF[0] = DFF[0];
+                    tmp_rp = rp;
+                    tmp_wp = wp;
+                end
+            end
+        end
     end
 
-    always @ (wp or rp or rst_n or ren or wen) begin
-        error = ((wp == 4'b1111) && rst_n && wen) || ((rp == 4'b1000) && rst_n && ren && !wen);
+    always @ (posedge clk) begin
+        DFF[7] <= tmp_DFF[7];
+        DFF[6] <= tmp_DFF[6];
+        DFF[5] <= tmp_DFF[5];
+        DFF[4] <= tmp_DFF[4];
+        DFF[3] <= tmp_DFF[3];
+        DFF[2] <= tmp_DFF[2];
+        DFF[1] <= tmp_DFF[1];
+        DFF[0] <= tmp_DFF[0];
+        rp <= tmp_rp;
+        wp <= tmp_wp;
+    end
+
+    always @ (*) begin
+        if (!rst_n) tmp_out = 8'd0;
+        else tmp_out = DFF[rp];
+    end
+
+    always @ (posedge clk) begin
+        dout <= tmp_out;
+    end
+
+    // error
+    always @ (*) begin
+        tmp_err = ((wp == 4'b1111) && rst_n && wen && !ren) || ((rp == 4'b1000) && rst_n && ren);
+    end
+
+    always @ (posedge clk) begin
+        error <= tmp_err;
     end
 
 
