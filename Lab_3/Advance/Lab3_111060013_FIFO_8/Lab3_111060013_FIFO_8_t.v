0575dabdab6cd867ac73751d61206f79ed573bde
liuutin9 <136707519+liuutin9@users.noreply.github.com>
2023-10-25 21:26:43 +0800

    畫完圖了

--- Lab_3/Advance/Lab3_111060013_FIFO_8/Lab3_111060013_FIFO_8_t.v
+++ Lab_3/Advance/Lab3_111060013_FIFO_8/Lab3_111060013_FIFO_8_t.v
@@ -1,4 +1,3 @@
-/*
 `timescale 1ns / 1ps
 
 module Lab3_111060013_FIFO_8_t;
@@ -19,165 +18,64 @@ module Lab3_111060013_FIFO_8_t;
     always # (1) clk = ~clk;
     
     initial begin
-        #1 rst_n <= 1'b0;
-           din <= 8'b00000000;
-           ren <= 1'b1;
-           wen <= 1'b0;
-        #2 rst_n <= 1'b1;
-        #2 ren <= 1'b0;
-           wen <= 1'b1;
-           din <= 8'd56;
-        #2 din <= 8'd11;
-        #2 din <= 8'd42;
-        #2 din <= 8'd10;
-        #2 din <= 8'd23;
-        #2 din <= 8'd20;
-        #2 din <= 8'd6;
-        #2 din <= 8'd85;
-        #2 din <= 8'd45;
-           ren <= 1'b1;
-        #2 din <= 8'd12;
-           ren <= 1'b0;
-        #2 din <= 8'd77;
-        #2 wen <= 0;
-           din <= 8'd0;
-        #2 $finish;
+        @(negedge clk)
+            rst_n <= 1'b0;
+            din <= 8'b00000000;
+            ren <= 1'b1;
+            wen <= 1'b0;
+
+        @(negedge clk)
+            rst_n <= 1'b1;
+
+        @(negedge clk)
+            ren <= 1'b0;
+            wen <= 1'b1;
+            din <= 8'd56;
+
+        @(negedge clk)
+            din <= 8'd11;
+
+        @(negedge clk)
+            din <= 8'd42;
+
+        @(negedge clk)
+            din <= 8'd10;
+
+        @(negedge clk)
+            din <= 8'd23;
+
+        @(negedge clk)
+            din <= 8'd20;
+
+        @(negedge clk)
+            din <= 8'd6;
+
+        @(negedge clk)
+            din <= 8'd85;
+
+        @(negedge clk)
+            din <= 8'd45;
+            ren <= 1'b1;
+
+        @(negedge clk)
+            din <= 8'd12;
+            ren <= 1'b0;
+
+        @(negedge clk)
+            din <= 8'd77;
+
+        @(negedge clk)
+            wen <= 0;
+            din <= 8'd0;
+
+        repeat (10) begin
+            @(negedge clk)
+                ren <= 1;
+        end
+
+        @(negedge clk)
+            $finish;
+
     end
-
-endmodule
-*/
-
-`timescale 1ns / 1ps
-
-module Lab3_Team30_FIFO_8_t;
-
-reg ren = 1'b0, wen = 1'b0, rst_n = 1'b1, clk = 1'b0;
-reg [7:0] din = 8'd0;
-wire [7:0] dout;
-wire error;
-
-always#(5)clk = !clk;
-
-FIFO_8 m(clk, rst_n, wen, ren, din, dout, error);
-initial begin
-    @ (negedge clk)
-	rst_n = 1'b0;
-	@ (negedge clk)
-	rst_n = 1'b1;
-    @(negedge clk)
-    din = 8'd10;
-    ren = 1'b0;
-    wen = 1'b1;
-    @(negedge clk)
-    din = 8'd20;
-    ren = 1'b0;
-    wen = 1'b1;
-    @(negedge clk)
-    din = 8'd30;
-    ren = 1'b0;
-    wen = 1'b1;
-    @(negedge clk)
-    din = 8'd0;
-    ren = 1'b1;
-    wen = 1'b0;
-    @(negedge clk)
-    din = 8'd0;
-    ren = 1'b1;
-    wen = 1'b1;
-    @(negedge clk)
-    din = 8'd0;
-    ren = 1'b1;
-    wen = 1'b1;
-    @(negedge clk)
-    din = 8'd0;
-    ren = 1'b1;
-    wen = 1'b0;
-    @(negedge clk)
-    din = 8'd0;
-    ren = 1'b0;
-    wen = 1'b0;
-    @(negedge clk)
-    din = 8'd5;
-    ren = 1'b0;
-    wen = 1'b1;
-    @(negedge clk)
-    din = 8'd10;
-    ren = 1'b0;
-    wen = 1'b1;
-    @(negedge clk)
-    din = 8'd15;
-    ren = 1'b0;
-    wen = 1'b1;
-    @(negedge clk)
-    din = 8'd20;
-    ren = 1'b0;
-    wen = 1'b1;
-    @(negedge clk)
-    din = 8'd25;
-    ren = 1'b0;
-    wen = 1'b1;
-    @(negedge clk)
-    din = 8'd30;
-    ren = 1'b0;
-    wen = 1'b1;
-    @(negedge clk)
-    din = 8'd35;
-    ren = 1'b0;
-    wen = 1'b1;
-    @(negedge clk)
-    din = 8'd40;
-    ren = 1'b0;
-    wen = 1'b1;
-    @(negedge clk)
-    din = 8'd45;
-    ren = 1'b0;
-    wen = 1'b1;
-    @(negedge clk)
-    din = 8'd50;
-    ren = 1'b0;
-    wen = 1'b1;
-    @(negedge clk)
-    din = 8'd0;
-    ren = 1'b1;
-    wen = 1'b0;
-    @(negedge clk)
-    din = 8'd0;
-    ren = 1'b1;
-    wen = 1'b0;
-    @(negedge clk)
-    din = 8'd0;
-    ren = 1'b1;
-    wen = 1'b0;
-    @(negedge clk)
-    din = 8'd0;
-    ren = 1'b1;
-    wen = 1'b1;
-    @(negedge clk)
-    din = 8'd0;
-    ren = 1'b1;
-    wen = 1'b1;
-    @(negedge clk)
-    din = 8'd0;
-    ren = 1'b1;
-    wen = 1'b1;
-    @(negedge clk)
-    din = 8'd0;
-    ren = 1'b1;
-    wen = 1'b0;
-    @(negedge clk)
-    din = 8'd0;
-    ren = 1'b1;
-    wen = 1'b0;
-    @(negedge clk)
-    din = 8'd0;
-    ren = 1'b1;
-    wen = 1'b0;
-    @(negedge clk)
-    din = 8'd0;
-    ren = 1'b0;
-    wen = 1'b0;
-    @(negedge clk)
-    $finish;
-end
 
 endmodule
