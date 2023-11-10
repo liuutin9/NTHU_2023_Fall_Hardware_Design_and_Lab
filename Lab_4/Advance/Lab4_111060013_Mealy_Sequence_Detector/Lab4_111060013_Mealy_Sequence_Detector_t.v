9efc6d17708293403724ac9bfd5e3a204ba2c6bf
liuutin9 <136707519+liuutin9@users.noreply.github.com>
2023-11-10 19:32:08 +0800

    change tb

--- Lab_4/Advance/Lab4_111060013_Mealy_Sequence_Detector/Lab4_111060013_Mealy_Sequence_Detector_t.v
+++ Lab_4/Advance/Lab4_111060013_Mealy_Sequence_Detector/Lab4_111060013_Mealy_Sequence_Detector_t.v
@@ -5,14 +5,13 @@ module Lab4_111060013_Mealy_Sequence_Detector_t;
     reg rst_n = 1'b1;
     reg in = 1'b0;
     wire dec;
-    reg [3:0] counter = 4'd0; // 系傳
 
     Mealy_Sequence_Detector MSD(clk, rst_n, in, dec);
 
     always # (1) clk = ~clk;
 
     initial begin
-/*
+        
         @ (negedge clk)
             rst_n <= 1'b0;
             in <= 1'b0;
@@ -83,51 +82,7 @@ module Lab4_111060013_Mealy_Sequence_Detector_t;
 
         @ (negedge clk)
             $finish;
-*/
-        @(negedge clk)
-        rst_n = 1'b0;
-        @(negedge clk)
-        rst_n = 1'b1;
-        in = 1'b0;
-        @(negedge clk)
-        in = 1'b1;
-        @(negedge clk)
-        @(negedge clk)
-        @(negedge clk)
-        in = 1'b0;
-        @(negedge clk)
-        in = 1'b1;
-        @(negedge clk)
-        @(negedge clk)
-        in = 1'b0;
-        @(negedge clk)
-        in = 1'b1;
-        @(negedge clk)
-        in = 1'b0;
-        @(negedge clk)
-        @(negedge clk)
-        in = 1'b1;
-        @(negedge clk)
-        in = 1'b0;
-        @(negedge clk)
-        in = 1'b1;
-        @(negedge clk)
-        in = 1'b0;
-        @(negedge clk)
-        @(negedge clk)
-        repeat(16) begin
-            in = counter[0];
-            @(posedge clk)
-            in = counter[1];
-            @(posedge clk)
-            in = counter[2];
-            @(posedge clk)
-            in = counter[3];
-            @(posedge clk)
-            counter = counter + 1'b1;
-        end
-        @(negedge clk)
-        $finish;
+
     end
 
 endmodule
