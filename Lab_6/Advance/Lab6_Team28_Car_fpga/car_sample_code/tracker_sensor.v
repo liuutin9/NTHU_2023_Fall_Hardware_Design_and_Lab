c56b53e4038401566f190a7dc6be651ce972eb70
liuutin9 <liuutin9@gmail.com>
2023-12-13 19:51:18 +0800

    更新邏輯

--- Lab_6/Advance/Lab6_Team28_Car_fpga/car_sample_code/tracker_sensor.v
+++ Lab_6/Advance/Lab6_Team28_Car_fpga/car_sample_code/tracker_sensor.v
@@ -15,7 +15,7 @@ module tracker_sensor(clk, reset, left_signal, right_signal, mid_signal, state);
 
     reg [2:0] next_state;
     reg [22:0] counter;
-    reg [2:0] strong_state, next_strong_state;
+    // reg [2:0] strong_state, next_strong_state;
     // [TO-DO] Receive three signals and make your own policy.
     // Hint: You can use output state to change your action.
 /*
@@ -27,47 +27,50 @@ module tracker_sensor(clk, reset, left_signal, right_signal, mid_signal, state);
     always @ (posedge clk) begin
         if (reset==1'd1) begin
             state <= STOP;
-            strong_state <= 3'd0;
+            // strong_state <= 3'd0;
         end
         else begin
             state <= next_state;
-            strong_state <= next_strong_state;
+            // strong_state <= next_strong_state;
         end
     end
     always @ (*) begin
         //if (counter == 23'd0) begin
             case ({left_signal, mid_signal, right_signal})
                 3'b000: begin
-                    next_state = strong_state;
-                    next_strong_state = strong_state;
+                    if (state == STRONG_LEFT || state == STRONG_RIGHT) next_state = /*strong_state*/state;
+                    else if (state == LEFT) next_state = STRONG_LEFT;
+                    else if (state == RIGHT) next_state = STRONG_RIGHT;
+                    else next_state = BACK;
+                    // next_strong_state = strong_state;
                 end
                 3'b001: begin
                     next_state = STRONG_RIGHT;
-                    next_strong_state = STRONG_RIGHT;
+                    // next_strong_state = STRONG_RIGHT;
                 end
                 3'b011: begin
                     next_state = RIGHT;
-                    next_strong_state = strong_state;
+                    // next_strong_state = strong_state;
                 end
                 3'b010: begin
                     next_state = FOWARD;
-                    next_strong_state = strong_state;
+                    // next_strong_state = strong_state;
                 end
                 3'b111: begin
                     next_state = FOWARD;
-                    next_strong_state = strong_state;
+                    // next_strong_state = strong_state;
                 end
                 3'b110: begin
                     next_state = LEFT;
-                    next_strong_state = strong_state;
+                    // next_strong_state = strong_state;
                 end
                 3'b100: begin
                     next_state = STRONG_LEFT;
-                    next_strong_state = STRONG_LEFT;
+                    // next_strong_state = STRONG_LEFT;
                 end
                 default: begin
                     next_state = BACK;
-                    next_strong_state = strong_state;
+                    // next_strong_state = strong_state;
                 end
             endcase/*
         end
